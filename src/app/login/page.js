92ec49e876443e325c25f9ae97df958ebe209309
brunobrandao01 <claudineibrandao373@gmail.com>
2025-12-08 22:13:01 -0300

    Create page.js

---
+++ src/app/login/page.js
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "sans-serif", color: "white", background: "#000", height: "100vh" }}>
+      <h1>Login</h1>
+      <p>Página de login funcionando.</p>
+    </main>
+  );
+}
